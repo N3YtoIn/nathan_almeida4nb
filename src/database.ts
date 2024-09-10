6cf9989a90fed2bf739c414d6b0655ef0e6127b8
Nathan Almeida <153780414+N3YtoIn@users.noreply.github.com>
2024-09-10 23:24:29 +0000

    TransferMain - Separação do levantamento de requisitos em uma nova Branch

---
+++ src/database.ts
@@ -0,0 +1,13 @@
+import { Pool } from 'pg';
+
+// Substitua pela sua string de conexão do Render.com
+const connectionString = 'postgresql://nasystem_5s8a_user:7MjLJf1Vw6y7vnZAmGvl1irBnu5YGb3f@dpg-crbovtij1k6c738e4ctg-a.oregon-postgres.render.com/nasystem_5s8a';
+
+const pool = new Pool({
+  connectionString,
+  ssl: {
+    rejectUnauthorized: false, // Permite conexões SSL não autorizadas
+  }
+});
+
+export default pool;
---
+++ src/migrations/migrations.ts
@@ -0,0 +1,22 @@
+import pool from '../database';
+
+const createUsersTable = async () => {
+  const client = await pool.connect();
+  try {
+    const queryText = `
+      CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        email VARCHAR(100) UNIQUE NOT NULL
+      );
+    `;
+    await client.query(queryText);
+    console.log('Tabela "users" criada com sucesso!');
+  } catch (err) {
+    console.error('Erro ao criar tabela:', err);
+  } finally {
+    client.release();
+  }
+};
+
+createUsersTable().then(() => process.exit(0));
